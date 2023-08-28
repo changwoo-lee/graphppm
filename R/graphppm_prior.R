2fc5dd7fc6063e7228910b95bfd4b9a84fdbcbdf
changwoo-lee <c.lee@tamu.edu>
2023-08-28 13:43:09 -0500

    igraph dependency 3

--- R/graphppm_prior.R
+++ R/graphppm_prior.R
@@ -22,9 +22,9 @@ graphppm_prior <- function(graph0, logcohesion, cohesion_param = NULL,
   # input check for graph0
   if(!igraph::is.igraph(graph0)) stop("graph must be an igraph object")
   E0 = igraph::as_edgelist(graph0, names =F)
-  rownames(E0) = 1:m
   n = igraph::vcount(graph0)
   m = igraph::ecount(graph0)
+  rownames(E0) = 1:m
   A0 = as_adj(graph0, sparse = F, names = F) # sparse = F, lookup speed is faster in non-sparse form
   g0 = igraph::graph_from_edgelist(E0, directed = F)
   if(igraph::components(g0)$no > 1) stop("base graph must be connected")
